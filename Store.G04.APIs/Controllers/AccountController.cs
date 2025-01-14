0276b2ab2377eb68d87b022ac053dca2d9640abc
amrabbas1 <amrabbas2003@gmail.com>
2025-01-15 00:48:22 +0200

    AddAcountController

---
+++ Store.G04.APIs/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Win32;
+using Store.G04.APIs.Errors;
+using Store.G04.core.Dtos.Auth;
+using Store.G04.core.Services.Contract;
+
+namespace Store.G04.APIs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public AccountController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        [HttpPost("login")]//Post : /api/Accounts/login
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await _userService.LoginAsync(loginDto);
+            if (user == null) return Unauthorized(new ApiErrorResponse(StatusCodes.Status401Unauthorized));
+            return Ok(user);
+        }
+        [HttpPost("register")]//Post : /api/Accounts/register
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+        {
+            var user = await _userService.RegisterAsync(registerDto);
+            if (user == null) return BadRequest(new ApiErrorResponse(StatusCodes.Status400BadRequest,"Invalid Signup"));
+            return Ok(user);
+        }
+    }
+}
