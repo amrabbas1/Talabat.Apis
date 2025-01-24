088f187d88a6e1999242c09ae8a2d3ac8fe6649c
amrabbas1 <amrabbas2003@gmail.com>
2025-01-24 21:47:45 +0200

    AddPaymentService

---
+++ Store.G04.Service/Services/Payments/PaymentService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.G04.Service.Services.Payments
+{
+    internal class PaymentService
+    {
+    }
+}
---
+++ Store.G04.core/Services.Contract/IPaymentService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.G04.core.Services.Contract
+{
+    internal interface IPaymentService
+    {
+    }
+}
